8a32b82200a3019b6e5702767754fa9616e89485
ycambro <yurgenmora03@gmail.com>
2024-11-15 12:06:18 -0600

    Proyecto dos terminado, solo falta saber que envia el archivo de salida

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Manfred Jones
 
 En la carpeta principal, es decir, en miCompilador, ejecutar el siguiente comando:
 
-- `compilar y ejecutar ant`: ant run ej.txt datos.txt
+- `compilar y ejecutar ant`: ant run -Dapplication.args="test.txt out.txt"
 
 
 > Cambia el nombre del ej.txt por el nombre del archivo con el código a probar, recuerda que este archivo debe encontrarse en la misma ruta que el proyecto, o más sencillo, en la misma ruta donde esta el ej.txt!
--- src/micompilador/MiCompilador.java
+++ src/micompilador/MiCompilador.java
@@ -20,33 +20,58 @@ public class MiCompilador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Uso: [NOMBRE DEL PROGRAMA] [ARCHIVO DE ENTRADA] [ARCHIVO DE SALIDA]");
+            return;
+        }
+
+        String input = leerArchivo(args[0]);
+        if (input == null || input.isEmpty()) {
+            return;
+        }
+        
+        CharStream charStream = CharStreams.fromString(input);
+
+        // Análisis léxico
+        LenguajeL2Lexer lexer = new LenguajeL2Lexer(charStream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        System.out.println("Confirmacion [Fase Lexica]: Analisis lexico completado con exito.");
+
+        // Análisis sintáctico
+        LenguajeL2Parser parser = new LenguajeL2Parser(tokens);
+        ParseTree tree = parser.programa();
+        System.out.println("Confirmacion [Fase Sintactica]: Analisis sintactico completado con exito.");
+        
+        // Imprimir el AST
+        //System.out.println(tree.toStringTree(parser));
+
+        if (parser.getNumberOfSyntaxErrors() == 0) {
+            // Análisis semántico
+            LenguajeL2SemanticAnalyzer analyzer = new LenguajeL2SemanticAnalyzer();
+            analyzer.visit(tree);
+            System.out.println("Confirmacion [Fase Semantica]: Analisis semantico completado con exito.");
+        } else {
+            System.out.println("Se encontraron errores de sintaxis; no se ejecutara el analisis semantico.");
+        }
+    }
+    
+    // Lee el archivo de entrada
+    private static String leerArchivo(String rutaArchivo) {
         try {
-            String input = new String(Files.readAllBytes(Paths.get("C:\\Users\\nuryb\\OneDrive\\Documentos\\NetBeansProjects\\miCompilador\\src\\micompilador\\test.txt")));
-            CharStream charStream = CharStreams.fromString(input);
-
-            // Análisis léxico
-            LenguajeL2Lexer lexer = new LenguajeL2Lexer(charStream);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            System.out.println("Confirmacion [Fase Lexica]: Analisis lexico completado con exito.");
-
-            // Análisis sintáctico
-            LenguajeL2Parser parser = new LenguajeL2Parser(tokens);
-            ParseTree tree = parser.programa();
-            System.out.println("Confirmacion [Fase Sintactica]: Analisis sintactico completado con exito.");
-            
-            // Imprimir el AST
-            //System.out.println(tree.toStringTree(parser));
-
-            if (parser.getNumberOfSyntaxErrors() == 0) {
-                // Análisis semántico
-                LenguajeL2SemanticAnalyzer analyzer = new LenguajeL2SemanticAnalyzer();
-                analyzer.visit(tree);
-                System.out.println("Confirmacion [Fase Semantica]: Analisis semantico completado con exito.");
-            } else {
-                System.out.println("Se encontraron errores de sintaxis; no se ejecutara el analisis semantico.");
-            }
+            return new String(Files.readAllBytes(Paths.get(rutaArchivo)));
         } catch (IOException e) {
-            System.err.println("Error al leer el archivo pruebas.txt: " + e.getMessage());
+            System.out.println("Intentando leer archivo desde la ruta: " + Paths.get(rutaArchivo).toAbsolutePath());
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+            return null;
+        }
+    }
+
+    // Guarda el archivo de salida
+    private static void guardarArchivo(String archivoGuardado, String contenido) {
+        try {
+            Files.write(Paths.get(archivoGuardado), contenido.getBytes());
+        } catch (IOException e) {
+            System.err.println("Error al guardar el archivo: " + e.getMessage());
         }
     }
     
